1224364fb302faeb29f4d127329a1b9414a2068d
Juan S. Landy <jlandyr@gmail.com>
2021-04-27 17:55:08 +0200

    1. Removed forced unwrap !

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -32,7 +32,7 @@ public final class CoreDataFeedStore: FeedStore {
 		let context = self.context
 		context.perform {
 			do {
-				let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+				let request = NSFetchRequest<ManagedCache>(entityName: "\(ManagedCache.self)")
 				request.returnsObjectsAsFaults = false
 				if let cache = try context.fetch(request).first {
 					completion(.found(feed: cache.feed.compactMap { ($0 as? ManagedFeedImage) }.map { LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url) }, timestamp: cache.timestamp))
