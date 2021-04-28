7d31bb29b4b80b0e50d55d8d95391484fdc32ca0
Juan S. Landy <jlandyr@gmail.com>
2021-04-28 15:52:46 +0200

    NSFetchRequest replaced with helper method ManagedCache.find

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -34,7 +34,7 @@ public final class CoreDataFeedStore: FeedStore {
 			do {
 				let request = NSFetchRequest<ManagedCache>(entityName: "\(ManagedCache.self)")
 				request.returnsObjectsAsFaults = false
-				if let cache = try context.fetch(request).first {
+				if let cache = try ManagedCache.find(in: context) {
 					completion(.found(feed: cache.feed.compactMap { ($0 as? ManagedFeedImage) }.map { LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url) }, timestamp: cache.timestamp))
 				} else {
 					completion(.empty)
